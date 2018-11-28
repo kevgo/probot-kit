1b37c09041b8dc5d197780d22e331a2b54f21673
Kevin Goslar <kevin.goslar@gmail.com>
2018-11-28 15:45:36 -0600

    Exports

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import getBranchName from './get-branch-name'
+import getRepoBranchName from './get-repo-branch-name'
+import iterateCurrentCommitFiles from './iterate-current-commit-files'
+import loadBotConfig from './load-bot-config'
+import loadFile from './load-file'
+import updateFile from './update-file'
+
+export {
+  getBranchName,
+  getRepoBranchName,
+  iterateCurrentCommitFiles,
+  loadBotConfig,
+  loadFile,
+  updateFile
+}
