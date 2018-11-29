926f2e07702383cb947167d62ddda320fe4d1e9d
Kevin Goslar <kevin.goslar@gmail.com>
2018-11-28 19:27:59 -0800

    Fix getRepoBranchName

--- src/get-repo-branch-name.ts
+++ src/get-repo-branch-name.ts
@@ -5,5 +5,5 @@ import getRepoName from './get-repo-name'
 // Returns the name of the repository in which the activity described by the context happens,
 // in the format {repoName}/{branchName}.
 export default function(context: Context): string {
-  return getRepoName + '/' + getBranchName(context)
+  return getRepoName(context) + '/' + getBranchName(context)
 }
