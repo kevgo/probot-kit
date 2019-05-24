5c3e8a91ddcb42549ed067a121cc250ec8dd64ee
Kevin Goslar <kevin.goslar@gmail.com>
2019-05-24 09:26:52 -0400

    Got

--- src/download-code.ts
+++ src/download-code.ts
@@ -1,33 +1,36 @@
-import { https } from 'follow-redirects'
+import got from 'got'
 import gunzip from 'gunzip-maybe'
 import os from 'os'
 import path from 'path'
 import tar from 'tar-stream'
-import { createRequestData } from './download-code-helpers/create-request-data'
-import { TarUnpacker } from './download-code-helpers/unpacker'
+import { TarUnpacker } from './download-helpers/unpacker'
 
 // Downloads the given SHA and extracts it into a tmp directory.
 // Returns the directory if successful.
-export function downloadCode(
+export async function downloadCode(
   organization,
   repository,
   sha,
   debug
 ): Promise<string> {
-  console.log('DOWNLOADING:', sha)
+  console.log('DOWNLOADING CODE:', sha)
+  const url = `https://api.github.com/repos/${organization}/${repository}/tarball/${sha}`
+  const responseStream = await got.stream(url, {
+    headers: {
+      Accept: 'application/vnd.github.v3.raw',
+      Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
+      'User-Agent': 'Probot-Kit',
+      followRedirect: true
+    }
+  })
+  const tarExtractor = tar.extract()
+  const unpacker = new TarUnpacker(debug)
+  tarExtractor.on('entry', unpacker.unpackEntry.bind(unpacker))
   return new Promise((resolve, reject) => {
-    const requestData = createRequestData(organization, repository, sha)
-    // TODO: make async
-    const request = https.get(requestData, res => {
-      const tarExtractor = tar.extract()
-      const unpacker = new TarUnpacker(debug)
-      tarExtractor.on('entry', unpacker.unpackEntry.bind(unpacker))
-      tarExtractor.on('error', reject)
-      tarExtractor.on('finish', function() {
-        resolve(path.join(os.tmpdir(), `${organization}-${repository}-${sha}`))
-      })
-      res.pipe(gunzip()).pipe(tarExtractor)
+    tarExtractor.on('error', reject)
+    tarExtractor.on('finish', () => {
+      resolve(path.join(os.tmpdir(), `${organization}-${repository}-${sha}`))
     })
-    request.on('error', reject)
+    responseStream.pipe(gunzip()).pipe(tarExtractor)
   })
 }
--- src/download-pullrequest-patch.ts
+++ src/download-pullrequest-patch.ts
@@ -1,4 +1,4 @@
-import request from 'request-promise-native'
+import got from 'got'
 
 // downloads the patch for the given pull request
 export async function downloadPullrequestPatch(
@@ -6,14 +6,13 @@ export async function downloadPullrequestPatch(
   repo: string,
   pullRequestNumber: string
 ): Promise<string> {
-  const requestData = {
+  const url = `https://api.github.com/repos/${org}/${repo}/pulls/${pullRequestNumber}`
+  const response = await got(url, {
     headers: {
       Accept: 'application/vnd.github.v3.diff',
       Authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
       'User-Agent': 'Markdown-check-bot'
-    },
-    uri: `https://api.github.com/repos/${org}/${repo}/pulls/${pullRequestNumber}`
-  }
-  const patchText = await request(requestData)
-  return patchText
+    }
+  })
+  return response.body
 }
