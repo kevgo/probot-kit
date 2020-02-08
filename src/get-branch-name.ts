72a4e0888918500b26734ab3520e575b66357f6a
Kevin Goslar <kevin.goslar@gmail.com>
2020-02-08 14:18:13 -0600

    Remove pointless features

--- src/get-branch-name.ts
+++
@@ -1,9 +0,0 @@
-import webhooks from "@octokit/webhooks"
-import * as probot from "probot"
-
-/** Determines the name of the branch involved in the given Github event. */
-export function getBranchName(
-  context: probot.Context<webhooks.WebhookPayloadPush>
-): string {
-  return context.payload.ref.replace("refs/heads/", "")
-}
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,5 @@
-import { currentCommitFiles } from "./current-commit-files"
 import { downloadCode } from "./download-code"
 import { downloadPullrequestPatch } from "./download-pullrequest-patch"
 import { findPatchLine } from "./find-patch-line"
-import { getBranchName } from "./get-branch-name"
-import { getCommitAuthorName } from "./get-commit-author-name"
-import { getRepoBranchSha } from "./get-repo-branch-sha"
-import { getRepoName } from "./get-repo-name"
-import { getSha } from "./get-sha"
-import { loadBotConfig } from "./load-bot-config"
-import { loadFile, LoadFileResult } from "./load-file"
-import { updateFile } from "./update-file"
 
-export {
-  currentCommitFiles,
-  downloadCode,
-  downloadPullrequestPatch,
-  findPatchLine,
-  getCommitAuthorName,
-  getBranchName,
-  getRepoBranchSha,
-  getRepoName,
-  getSha,
-  loadBotConfig,
-  loadFile,
-  LoadFileResult,
-  updateFile
-}
+export { downloadCode, downloadPullrequestPatch, findPatchLine }
